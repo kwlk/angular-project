b8182ff299dec2d29125ba4261dc8bbff04ec0d8
kwlk <emiliakwolek@poczta.fm>
2020-01-11 12:32:35 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-bsgq8s
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-bsgq8s)
---
+++ src/app/course.ts
@@ -0,0 +1,26 @@
+export const product [
+  {
+  name: 'Analiza';
+  ETCS: "6";
+  semestr: '1';
+  forma: "wykład"
+  maxStudents: "200";
+  ocena: "4"
+  },
+  {
+  name: 'Analiza';
+  ETCS: "6";
+  semestr: '1';
+  forma: "wykład"
+  maxStudents: "200";
+  ocena: "4"
+  },
+  {
+  name: 'Analiza';
+  ETCS: "6";
+  semestr: '1';
+  forma: "wykład"
+  maxStudents: "200";
+  ocena: "4"
+  }
+];
